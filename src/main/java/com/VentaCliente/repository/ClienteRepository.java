4459e8398c0026cc661dc5be35432cf2926a883c
Carlos Eugenio Quiroz Balarezo <cquiroz@imarpe.gob.pe>
2025-07-09 10:15:24 -0500

    fix: Se insertan los clientes

--- src/main/java/com/VentaCliente/repository/ClienteRepository.java
+++ src/main/java/com/VentaCliente/repository/ClienteRepository.java
@@ -1,8 +1,12 @@
 package com.VentaCliente.repository;
 
+import com.VentaCliente.login.Usuario;
 import com.VentaCliente.model.Cliente;
 import com.VentaCliente.model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByNombre(String cliente);
 }
